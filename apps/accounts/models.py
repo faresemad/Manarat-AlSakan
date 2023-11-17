0e4460e7b163e58dca83905db0a881e8a7ee0d66
Fares Emad <faresemadx@gmail.com>
2023-11-17 18:21:08 +0200

    feat: add some edition to User model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -7,6 +9,7 @@ class User(AbstractUser):
         BUYER = "buyer", "Buyer"
         SELLER = "seller", "Seller"
 
+    id = models.UUIDField(primary_key=True, unique=True, editable=False, default=uuid.uuid4)
     name = models.CharField(max_length=100, unique=True)
     national_id = models.CharField(max_length=100, unique=True)
     email = models.EmailField(max_length=100, unique=True)
@@ -15,3 +18,10 @@ class User(AbstractUser):
     role = models.CharField(max_length=100, choices=UserTypes.choices, default=UserTypes.BUYER)
     USERNAME_FIELD = "national_id"
     REQUIRED_FIELDS = ["name", "email", "phone", "role"]
+
+    class Meta:
+        verbose_name_plural = "Users"
+        unique_together = ["email", "national_id"]
+        indexes = [
+            models.Index(fields=["id", "name"]),
+        ]
