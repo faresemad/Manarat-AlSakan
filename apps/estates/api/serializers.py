7272cd7a292709c716c840c37683ac44d0424a44
Fares Emad <faresemadx@gmail.com>
2023-11-19 00:33:50 +0200

    feat: craete `serializers` for Estates

---
+++ apps/estates/api/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+
+from apps.estates.models import Estate, EstateImage
+
+
+class EstateImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EstateImage
+        fields = ("image",)
+
+
+class EstateSerializer(serializers.ModelSerializer):
+    images = EstateImageSerializer(many=True, read_only=True)
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    upload_images = serializers.ListField(
+        child=serializers.ImageField(),
+        write_only=True,
+        required=False,
+    )
+
+    class Meta:
+        model = Estate
+        fields = (
+            "id",
+            "user",
+            "name",
+            "location",
+            "owner",
+            "description",
+            "slug",
+            "num_units",
+            "architecturally_designed",
+            "construction_date",
+            "request_state",
+            "images",
+            "upload_images",
+        )
+        read_only_fields = (
+            "id",
+            "slug",
+            "request_state",
+            "images",
+        )
+
+    def create(self, validated_data):
+        images = validated_data.pop("upload_images", [])
+        estate = Estate.objects.create(**validated_data)
+        for image in images:
+            EstateImage.objects.create(estates=estate, image=image)
+        return estate
