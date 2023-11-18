63b5b9ad0e6ce572cd3f3bc662fa14bdff934260
Fares Emad <faresemadx@gmail.com>
2023-11-19 00:26:27 +0200

    feat: create Estates models

--- apps/estates/admin.py
+++ apps/estates/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin  # noqa
 
+from apps.estates.models import Building, BuildingImage
+
 # Register your models here.
+
+admin.site.register(Building)
+admin.site.register(BuildingImage)
--- apps/estates/models.py
+++ apps/estates/models.py
@@ -1,3 +1,51 @@
-from django.db import models  # noqa
+import uuid
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Building(models.Model):
+    class RequestState(models.TextChoices):
+        PENDING = "PENDING"
+        APPROVED = "APPROVED"
+        REJECTED = "REJECTED"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    owner = models.CharField(max_length=255)
+    description = models.JSONField()
+    slug = models.SlugField(max_length=255, unique=True)
+    num_units = models.IntegerField()
+    architecturally_designed = models.BooleanField(default=True)
+    construction_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    request_state = models.CharField(max_length=10, choices=RequestState.choices, default=RequestState.PENDING)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Building"
+        verbose_name_plural = "Buildings"
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(fields=["id", "name", "location"]),
+        ]
+        unique_together = ["name", "location"]
+
+
+class BuildingImage(models.Model):
+    building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to="building_images/")
+
+    def __str__(self):
+        return self.building.name
+
+    class Meta:
+        verbose_name_plural = "Building Images"
+        verbose_name = "Building Image"
