322831c7e3ec14d6fdaea8ac374630790da3de66
Fares Emad <faresemadx@gmail.com>
2023-11-23 15:54:54 +0200

    feat: add permissions  in `estates` views

--- apps/estates/api/views.py
+++ apps/estates/api/views.py
@@ -2,8 +2,18 @@ from rest_framework import viewsets
 
 from apps.estates.api.serializers import EstateSerializer
 from apps.estates.models import Estate
+from apps.utils.permissions import IsOwnerOrReadOnly, IsSeller
 
 
 class EstatsViewSet(viewsets.ModelViewSet):
     queryset = Estate.objects.all()
     serializer_class = EstateSerializer
+
+    def get_permissions(self):
+        if self.action == "create":
+            permission_classes = [IsSeller]
+        elif self.action in ["update", "partial_update", "destroy"]:
+            permission_classes = [IsOwnerOrReadOnly]
+        else:
+            permission_classes = []
+        return [permission() for permission in permission_classes]
--- apps/utils/permissions.py
+++ apps/utils/permissions.py
@@ -1,11 +1,17 @@
+from django.contrib.auth.models import AnonymousUser
 from rest_framework.permissions import IsAuthenticated
 
 
 class IsSeller(IsAuthenticated):
     def has_permission(self, request, view):
-        return request.user.role == "seller"
+        return getattr(request.user, "role", None) == "seller"
 
 
 class IsBuyer(IsAuthenticated):
     def has_permission(self, request, view):
-        return request.user.role == "buyer"
+        return getattr(request.user, "role", None) == "buyer"
+
+
+class IsOwnerOrReadOnly(IsAuthenticated):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user or isinstance(request.user, AnonymousUser)
